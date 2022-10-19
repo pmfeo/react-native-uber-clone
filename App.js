79e4396e4cd5e82d3f5af82ad6ba05523e2281be
pmfeo <pablo.feo.acosta@gmail.com>
2022-10-19 12:50:30 -0300

    Add prettier config. Add Redux and RTK.

--- App.js
+++ App.js
@@ -1,17 +1,16 @@
 import { StatusBar } from 'expo-status-bar';
 import { Text, View } from 'react-native';
-// import { NativeWindStyleSheet } from "nativewind";
-
+import { Provider } from 'react-redux';
+import { store } from './store';
 
 export default function App() {
   return (
-    <View className="flex-1 items-center justify-center bg-white">
-      <Text className="text-blue-900">Open up App.js to start working on app</Text>
-      <StatusBar style="auto" />
-    </View>
+    <Provider store={store}>
+      <View className="flex-1         items-center       justify-center 		bg-white">
+        <Text className="text-blue-900">Open up App.js to staaaart working on app</Text>
+
+        <StatusBar style="auto" />
+      </View>
+    </Provider>
   );
 }
-
-// NativeWindStyleSheet.setOutput({
-//   default: "native",
-// });
